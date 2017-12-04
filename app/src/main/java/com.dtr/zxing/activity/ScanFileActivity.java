1759c853b19c50b0d3a84fcd4e290a9c3cc57f55
EsdeathYZH <1162526220@qq.com>
2017-12-04 21:58:26 +0800

    Fix some bugs.

--- app/src/main/java/com.dtr/zxing/activity/ScanFileActivity.java
+++ app/src/main/java/com.dtr/zxing/activity/ScanFileActivity.java
@@ -19,6 +19,8 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.dtr.zxing.decode.DecodeFormatManager;
 import com.dtr.zxing.decode.DecodeThread;
@@ -38,6 +40,7 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumMap;
@@ -51,12 +54,12 @@ import java.util.Map;
 public class ScanFileActivity extends Activity implements SurfaceHolder.Callback{
     private ArrayList<Box> boxes;
     InputStream xmlFile;
-    private SurfaceView backPreview;
+    private ImageView backPreview;
     private SurfaceView drawPreview;
     private Bitmap imageFile;
     private final MultiFormatReader multiFormatReader=new MultiFormatReader();
-    ArrayList<PlanarYUVLuminanceSource> planarYUVLuminanceSources;
-    ArrayList<Result> rawResults;
+    private ArrayList<PlanarYUVLuminanceSource> planarYUVLuminanceSources;
+    private ArrayList<Result> rawResults;
     Map<DecodeHintType, Object> hints;
     private boolean isHasSurface;
     class Box{
@@ -71,27 +74,50 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_scanfile);
-        drawPreview=(SurfaceView)findViewById(R.id.draw_preview_file);
-        backPreview=(SurfaceView)findViewById(R.id.code_background);
-        backPreview.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-        imageFile= BitmapFactory.decodeResource(getResources(), R.drawable.qrcode);
+        //drawPreview=(SurfaceView)findViewById(R.id.draw_preview_file);
+        backPreview=(ImageView)findViewById(R.id.code_background);
+        //drawPreview.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize=4;
+        imageFile= BitmapFactory.decodeResource(getResources(), R.drawable.qrcode4,options).copy(Bitmap.Config.ARGB_8888, true);;
+        //Bitmap scaleImage=Bitmap.createScaledBitmap(imageFile,(int)(imageFile.getWidth()*0.4),(int)(imageFile.getHeight()*0.4),true);
+
+        Toast.makeText(this,"Width:"+imageFile.getWidth()
+                +",Height:"+imageFile.getHeight(),Toast.LENGTH_SHORT).show();
         initHint();
         multiFormatReader.setHints(hints);
-        xmlFile= getResources().openRawResource(R.raw.code_info);
+        xmlFile= getResources().openRawResource(R.raw.bboxinfo4);
         try {
-            //initBoxes();
+            initBoxes();
         }catch(Exception e){
             e.printStackTrace();
         }
-        drawImage();
-        //drawRects();
-        /*decode();
-        drawResults();*/
+        //drawImage();
+        drawRects();
+        backPreview.setImageBitmap(imageFile);
+        decode();
+        //drawResults();
     }
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         if (!isHasSurface) {
             isHasSurface = true;
+        }
+        if (isHasSurface) {
+            // The activity was paused but not stopped, so the surface still
+            // exists. Therefore
+            // surfaceCreated() won't be called, so init the camera here.
+            //initCamera(scanPreview.getHolder());
+        } else {
+            // Install the callback and wait for surfaceCreated() to init the
+            // camera.
+            //drawPreview.getHolder().addCallback(this);
+            //drawPreview.getHolder().setFormat(PixelFormat.TRANSLUCENT);//My code
         }
     }
 
@@ -114,6 +140,7 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
         hints.put(DecodeHintType.POSSIBLE_FORMATS, decodeFormats);
     }
     private void initBoxes()throws Exception{
+        boxes=new ArrayList<Box>();
         Box box = new Box();
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         XmlPullParser pullParser = factory.newPullParser();
@@ -124,8 +151,10 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
                 case XmlPullParser.START_DOCUMENT:
                     break;
                 case XmlPullParser.START_TAG:
-                    if ("box".equals(pullParser.getName())){
-
+                    if ("data".equals(pullParser.getName())){
+                        break;
+                    }else if("_".equals(pullParser.getName())){
+                        break;
                     }else if ("x".equals(pullParser.getName())){
                         int x = Integer.parseInt(pullParser.nextText());
                         box.x=x;
@@ -138,9 +167,11 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
                     }
                     break;
                 case XmlPullParser.END_TAG:
-                    if ("box".equals(pullParser.getName())){
+                    if ("_".equals(pullParser.getName())){
                         boxes.add(box);
-
+                        box=new Box();
+                    }else if("data".equals(pullParser.getName())){
+                        return;
                     }
                     break;
             }
@@ -152,29 +183,40 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
         Size size = new Size(imageFile.getWidth(),imageFile.getHeight());
         //create byte[] from image file.
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        imageFile.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        imageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
 
+        int bytes = imageFile.getByteCount();
+        ByteBuffer buf = ByteBuffer.allocate(bytes);
+        imageFile.copyPixelsToBuffer(buf);
 
-        buildLuminanceSource(baos.toByteArray(), size.getWidth(), size.getHeight());
+        rawResults=new ArrayList<Result>();
+        Toast.makeText(this,"Width:"+size.getWidth()
+                +",Height:"+size.getHeight(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Length should be:"+imageFile.getByteCount()+"Length:"+buf.array().length,Toast.LENGTH_SHORT).show();
+
+        //buildLuminanceSource(baos.toByteArray(), size.getWidth(), size.getHeight());
+        buildLuminanceSource(buf.array(), size.getWidth(), size.getHeight());
+
         for(int i=0;i<boxes.size();i++){
-            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(planarYUVLuminanceSources.get(i)));
-            try {
+            //BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(planarYUVLuminanceSources.get(i)));
+            /*try {
                 Result result=multiFormatReader.decodeWithState(bitmap);
                 rawResults.add(result);
             } catch (ReaderException re) {
                 // continue
             } finally {
                 multiFormatReader.reset();
-            }
+            }*/
         }
     }
 
     private void drawResults(){
         Canvas canvas=null;
-        SurfaceHolder holder=drawPreview.getHolder();
+        //SurfaceHolder holder=drawPreview.getHolder();
         try{
-            synchronized (holder){
-                canvas=holder.lockCanvas();
+            //synchronized (holder){
+                canvas=new Canvas (imageFile) ;
+                //canvas=holder.lockCanvas();
                 Paint p = new Paint(); //创建画笔
                 p.setColor(Color.BLUE);
                 p.setTextSize(28);
@@ -183,49 +225,54 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
                     canvas.drawText(rawResults.get(i).getText(),boxes.get(i).x, boxes.get(i).y, p);//画出结果
                 }
 
-            }
+            //}
         }catch(Exception e){
             e.printStackTrace();
         }finally {
             if(canvas!=null){
-                holder.unlockCanvasAndPost(canvas);
+                //holder.unlockCanvasAndPost(canvas);
             }
         }
     }
     private void drawRects(){
         Canvas canvas=null;
-        SurfaceHolder holder=drawPreview.getHolder();
+        //SurfaceHolder holder=drawPreview.getHolder();
         try{
-            synchronized (holder){
-                canvas=holder.lockCanvas();
+            //synchronized (holder){
+                canvas=new Canvas (imageFile) ;
+                //canvas=holder.lockCanvas();
                 Paint p = new Paint(); //创建画笔
                 p.setColor(Color.RED);
                 p.setTextSize(28);
+                p.setStyle(Paint.Style.STROKE);
+                p.setStrokeWidth(3);
                 canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);//绘制透明色
                 for(int i=0;i<boxes.size();i++){
-                    canvas.drawRect(new Rect(boxes.get(i).x,boxes.get(i).y,
-                            boxes.get(i).x+boxes.get(i).width,
-                            boxes.get(i).y+boxes.get(i).width),p);//画出结果
+                    canvas.drawRect(new Rect((int)(boxes.get(i).x*15/8),(int)(boxes.get(i).y*15/8),
+                            (int)((boxes.get(i).x+boxes.get(i).width)*15/8),
+                            (int)((boxes.get(i).y+boxes.get(i).width)*15/8)),p);//画出结果
                 }
 
-            }
+            //}
         }catch(Exception e){
             e.printStackTrace();
         }finally {
             if(canvas!=null){
-                holder.unlockCanvasAndPost(canvas);
+                //holder.unlockCanvasAndPost(canvas);
             }
         }
     }
     private void buildLuminanceSource(byte[] data, int width, int height) {
+        planarYUVLuminanceSources=new ArrayList<PlanarYUVLuminanceSource>();
         for(int i=0;i<boxes.size();i++){
-            planarYUVLuminanceSources.add(new PlanarYUVLuminanceSource(data, width, height, boxes.get(i).x, boxes.get(i).y, boxes.get(i).width,boxes.get(i).width, false));
+            planarYUVLuminanceSources.add(new PlanarYUVLuminanceSource(data, width, height, boxes.get(i).x*15/8, boxes.get(i).y*15/8,
+                    boxes.get(i).width*15/8,boxes.get(i).width*15/8, false));
         }
     }
 
-    private void drawImage(){
+    /*private void drawImage(){
         Canvas canvas=null;
-        SurfaceHolder holder=backPreview.getHolder();
+        SurfaceHolder holder=drawPreview.getHolder();
         try{
             synchronized (holder){
                 canvas=holder.lockCanvas();
@@ -242,7 +289,7 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
                 holder.unlockCanvasAndPost(canvas);
             }
         }
-    }
+    }*/
 }
 
 
