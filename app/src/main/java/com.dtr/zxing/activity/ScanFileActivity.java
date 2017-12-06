ff63bd32ac5f3add2b19ee80b92bc03d9d9daa20
EsdeathYZH <1162526220@qq.com>
2017-12-06 23:24:03 +0800

    Add the function of scanning image file.

--- app/src/main/java/com.dtr/zxing/activity/ScanFileActivity.java
+++ app/src/main/java/com.dtr/zxing/activity/ScanFileActivity.java
@@ -29,8 +29,10 @@ import com.example.qrcode.R;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
+import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.ReaderException;
 import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
@@ -51,55 +53,60 @@ import java.util.Map;
  * Created by SHIYONG on 2017/11/23.
  */
 
-public class ScanFileActivity extends Activity implements SurfaceHolder.Callback{
+public class ScanFileActivity extends Activity implements SurfaceHolder.Callback {
     private ArrayList<Box> boxes;
     InputStream xmlFile;
     private ImageView backPreview;
     private SurfaceView drawPreview;
     private Bitmap imageFile;
-    private final MultiFormatReader multiFormatReader=new MultiFormatReader();
-    private ArrayList<PlanarYUVLuminanceSource> planarYUVLuminanceSources;
+    private final MultiFormatReader multiFormatReader = new MultiFormatReader();
+    private ArrayList<LuminanceSource> luminanceSources;
     private ArrayList<Result> rawResults;
     Map<DecodeHintType, Object> hints;
     private boolean isHasSurface;
-    class Box{
+    private Paint paint;
+    private Canvas canvas;
+    class Box {
         public int x;
         public int y;
         public int width;
 
     }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_scanfile);
-        //drawPreview=(SurfaceView)findViewById(R.id.draw_preview_file);
-        backPreview=(ImageView)findViewById(R.id.code_background);
-        //drawPreview.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize=4;
-        imageFile= BitmapFactory.decodeResource(getResources(), R.drawable.qrcode4,options).copy(Bitmap.Config.ARGB_8888, true);;
-        //Bitmap scaleImage=Bitmap.createScaledBitmap(imageFile,(int)(imageFile.getWidth()*0.4),(int)(imageFile.getHeight()*0.4),true);
+        backPreview = (ImageView) findViewById(R.id.code_background);
+        paint = new Paint(); //创建画笔
 
-        Toast.makeText(this,"Width:"+imageFile.getWidth()
-                +",Height:"+imageFile.getHeight(),Toast.LENGTH_SHORT).show();
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        //Bitmap rawFile=BitmapFactory.decodeResource(getResources(), R.drawable.qrcode4);
+        imageFile = BitmapFactory.decodeResource(getResources(), R.drawable.qrcode4, options).copy(Bitmap.Config.ARGB_8888, true);;
+        canvas=new Canvas(imageFile);
+
+        Toast.makeText(this, "Width:" + imageFile.getWidth()
+                + ",Height:" + imageFile.getHeight(), Toast.LENGTH_SHORT).show();
+
         initHint();
         multiFormatReader.setHints(hints);
-        xmlFile= getResources().openRawResource(R.raw.bboxinfo4);
+        xmlFile = getResources().openRawResource(R.raw.bboxinfo4);
         try {
             initBoxes();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        //drawImage();
+        decode();
+        drawResults();
         drawRects();
         backPreview.setImageBitmap(imageFile);
-        decode();
-        //drawResults();
     }
+
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
     }
 
@@ -130,7 +137,8 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
     }
-    private void initHint(){
+
+    private void initHint() {
         hints = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
         Collection<BarcodeFormat> decodeFormats = new ArrayList<BarcodeFormat>();
         decodeFormats.addAll(EnumSet.of(BarcodeFormat.AZTEC));
@@ -139,38 +147,39 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
         decodeFormats.addAll(DecodeFormatManager.getQrCodeFormats());
         hints.put(DecodeHintType.POSSIBLE_FORMATS, decodeFormats);
     }
-    private void initBoxes()throws Exception{
-        boxes=new ArrayList<Box>();
+
+    private void initBoxes() throws Exception {
+        boxes = new ArrayList<Box>();
         Box box = new Box();
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         XmlPullParser pullParser = factory.newPullParser();
         pullParser.setInput(xmlFile, "UTF-8");
         int eventType = pullParser.getEventType();
-        while (eventType != XmlPullParser.END_DOCUMENT){
-            switch (eventType){
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            switch (eventType) {
                 case XmlPullParser.START_DOCUMENT:
                     break;
                 case XmlPullParser.START_TAG:
-                    if ("data".equals(pullParser.getName())){
+                    if ("data".equals(pullParser.getName())) {
                         break;
-                    }else if("_".equals(pullParser.getName())){
+                    } else if ("_".equals(pullParser.getName())) {
                         break;
-                    }else if ("x".equals(pullParser.getName())){
+                    } else if ("x".equals(pullParser.getName())) {
                         int x = Integer.parseInt(pullParser.nextText());
-                        box.x=x;
-                    }else if ("y".equals(pullParser.getName())){
+                        box.x = x;
+                    } else if ("y".equals(pullParser.getName())) {
                         int y = Integer.parseInt(pullParser.nextText());
-                        box.y=y;
-                    }else if ("w".equals(pullParser.getName())){
+                        box.y = y;
+                    } else if ("w".equals(pullParser.getName())) {
                         int width = Integer.parseInt(pullParser.nextText());
-                        box.width=width;
+                        box.width = width;
                     }
                     break;
                 case XmlPullParser.END_TAG:
-                    if ("_".equals(pullParser.getName())){
+                    if ("_".equals(pullParser.getName())) {
                         boxes.add(box);
-                        box=new Box();
-                    }else if("data".equals(pullParser.getName())){
+                        box = new Box();
+                    } else if ("data".equals(pullParser.getName())) {
                         return;
                     }
                     break;
@@ -179,8 +188,8 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
         }
     }
 
-    private void decode(){
-        Size size = new Size(imageFile.getWidth(),imageFile.getHeight());
+    private void decode() {
+        Size size = new Size(imageFile.getWidth(), imageFile.getHeight());
         //create byte[] from image file.
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         imageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
@@ -189,107 +198,77 @@ public class ScanFileActivity extends Activity implements SurfaceHolder.Callback
         ByteBuffer buf = ByteBuffer.allocate(bytes);
         imageFile.copyPixelsToBuffer(buf);
 
-        rawResults=new ArrayList<Result>();
-        Toast.makeText(this,"Width:"+size.getWidth()
-                +",Height:"+size.getHeight(),Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"Length should be:"+imageFile.getByteCount()+"Length:"+buf.array().length,Toast.LENGTH_SHORT).show();
-
+        rawResults = new ArrayList<Result>();
         //buildLuminanceSource(baos.toByteArray(), size.getWidth(), size.getHeight());
         buildLuminanceSource(buf.array(), size.getWidth(), size.getHeight());
 
-        for(int i=0;i<boxes.size();i++){
-            //BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(planarYUVLuminanceSources.get(i)));
-            /*try {
-                Result result=multiFormatReader.decodeWithState(bitmap);
+        for (int i = 0; i < boxes.size(); i++) {
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(luminanceSources.get(i)));
+            try {
+                Result result = multiFormatReader.decode(bitmap);
+                Toast.makeText(this, result.getText(), Toast.LENGTH_SHORT).show();
                 rawResults.add(result);
             } catch (ReaderException re) {
-                // continue
+                rawResults.add(null);
+                re.printStackTrace();
             } finally {
                 multiFormatReader.reset();
-            }*/
+            }
         }
     }
 
-    private void drawResults(){
-        Canvas canvas=null;
-        //SurfaceHolder holder=drawPreview.getHolder();
-        try{
-            //synchronized (holder){
-                canvas=new Canvas (imageFile) ;
-                //canvas=holder.lockCanvas();
-                Paint p = new Paint(); //创建画笔
-                p.setColor(Color.BLUE);
-                p.setTextSize(28);
-                canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);//绘制透明色
-                for(int i=0;i<boxes.size();i++){
-                    canvas.drawText(rawResults.get(i).getText(),boxes.get(i).x, boxes.get(i).y, p);//画出结果
+    private void drawResults() {
+        try {
+            paint.setColor(Color.WHITE);
+            paint.setTextSize(28);
+            for (int i = 0; i < boxes.size(); i++) {
+                if (rawResults.get(i) != null) {
+                    canvas.drawText(rawResults.get(i).getText(), boxes.get(i).x*15/8,
+                            boxes.get(i).y*15/8+boxes.get(i).width*15/16, paint);//画出结果
                 }
-
-            //}
-        }catch(Exception e){
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(canvas!=null){
+        } finally {
+            if (canvas != null) {
                 //holder.unlockCanvasAndPost(canvas);
             }
         }
     }
-    private void drawRects(){
-        Canvas canvas=null;
-        //SurfaceHolder holder=drawPreview.getHolder();
-        try{
-            //synchronized (holder){
-                canvas=new Canvas (imageFile) ;
-                //canvas=holder.lockCanvas();
-                Paint p = new Paint(); //创建画笔
-                p.setColor(Color.RED);
-                p.setTextSize(28);
-                p.setStyle(Paint.Style.STROKE);
-                p.setStrokeWidth(3);
-                canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);//绘制透明色
-                for(int i=0;i<boxes.size();i++){
-                    canvas.drawRect(new Rect((int)(boxes.get(i).x*15/8),(int)(boxes.get(i).y*15/8),
-                            (int)((boxes.get(i).x+boxes.get(i).width)*15/8),
-                            (int)((boxes.get(i).y+boxes.get(i).width)*15/8)),p);//画出结果
-                }
 
-            //}
-        }catch(Exception e){
+    private void drawRects() {
+        try {
+            paint.setColor(Color.RED);
+            paint.setTextSize(28);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(3);
+            for (int i = 0; i < boxes.size(); i++) {
+                canvas.drawRect(new Rect((int) (boxes.get(i).x * 15 / 8), (int) (boxes.get(i).y * 15 / 8),
+                        (int) ((boxes.get(i).x + boxes.get(i).width) * 15 / 8),
+                        (int) ((boxes.get(i).y + boxes.get(i).width) * 15 / 8)), paint);//画出结果
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(canvas!=null){
+        } finally {
+            if (canvas != null) {
                 //holder.unlockCanvasAndPost(canvas);
             }
         }
     }
+
     private void buildLuminanceSource(byte[] data, int width, int height) {
-        planarYUVLuminanceSources=new ArrayList<PlanarYUVLuminanceSource>();
-        for(int i=0;i<boxes.size();i++){
-            planarYUVLuminanceSources.add(new PlanarYUVLuminanceSource(data, width, height, boxes.get(i).x*15/8, boxes.get(i).y*15/8,
-                    boxes.get(i).width*15/8,boxes.get(i).width*15/8, false));
+        luminanceSources = new ArrayList<LuminanceSource>();
+
+        int[] pixels = new int[width * height];
+        imageFile.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        RGBLuminanceSource image = new RGBLuminanceSource(width, height, pixels);
+        for (int i = 0; i < boxes.size(); i++) {
+            luminanceSources.add(image.crop(boxes.get(i).x * 15 / 8, boxes.get(i).y * 15 / 8,
+                    boxes.get(i).width * 15 / 8, boxes.get(i).width * 15 / 8));
         }
     }
-
-    /*private void drawImage(){
-        Canvas canvas=null;
-        SurfaceHolder holder=drawPreview.getHolder();
-        try{
-            synchronized (holder){
-                canvas=holder.lockCanvas();
-                Paint p = new Paint(); //创建画笔
-                p.setColor(Color.RED);
-                p.setTextSize(28);
-                canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);//绘制透明色
-                canvas.drawBitmap(imageFile,0,0,p);
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally {
-            if(canvas!=null){
-                holder.unlockCanvasAndPost(canvas);
-            }
-        }
-    }*/
 }
+
 
 
